a78dc9af29e2c6e6050a644934ad5d836515016c
Oliver Kilk <oliver.kilk@tptlive.ee>
2018-03-28 09:25:09 +0300

    corrected appcenter shit

--- androidTest/androidTest/MainActivity.cs
+++ androidTest/androidTest/MainActivity.cs
@@ -11,13 +11,10 @@ namespace androidTest
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
-        {
+        { 
             base.OnCreate(savedInstanceState);
-            {
-                AppCenter.Start("60c350e3-3deb-473e-8e8f-8d70e53d4477", typeof(Analytics), typeof(Crashes));
-            }
 
-            base.OnCreate(savedInstanceState);
+            AppCenter.Start("60c350e3-3deb-473e-8e8f-8d70e53d4477", typeof(Analytics), typeof(Crashes));
 
             SetContentView(Resource.Layout.Main);
         }
