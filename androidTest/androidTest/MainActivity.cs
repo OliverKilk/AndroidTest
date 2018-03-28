e666e0e3ad0884eda6cb832c7dec6e23ef4ee59e
Oliver Kilk <oliver.kilk@tptlive.ee>
2018-03-28 09:23:35 +0300

    added appcenter shit

--- androidTest/androidTest/MainActivity.cs
+++ androidTest/androidTest/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace androidTest
 {
@@ -10,8 +13,12 @@ namespace androidTest
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            {
+                AppCenter.Start("60c350e3-3deb-473e-8e8f-8d70e53d4477", typeof(Analytics), typeof(Crashes));
+            }
 
-            // Set our view from the "main" layout resource
+            base.OnCreate(savedInstanceState);
+
             SetContentView(Resource.Layout.Main);
         }
     }
